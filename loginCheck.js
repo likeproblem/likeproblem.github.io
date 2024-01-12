85f28cba5a8fd4f79f0c19d157c93db0a9aecdef
likeproblem <75926463+likeproblem@users.noreply.github.com>
2024-01-12 08:26:06 -0800

    Update loginCheck.js

--- loginCheck.js
+++ loginCheck.js
@@ -1,10 +1,7 @@
-// Check if the cookie has the value "1234"
 function checkCookie() {
-  // If the cookie value is not "1234", redirect to /login.html
-  if (document.cookie !== "1234") {
+  if (document.cookie !== "GoodPassword123") {
     window.location.href = "/login.html";
   }
 }
 
-// Call the checkCookie function when the script is loaded
 checkCookie();
