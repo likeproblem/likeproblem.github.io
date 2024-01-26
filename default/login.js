d8454f5a973f95e40d8845c67aa8cb8462ca9632
likeproblem <75926463+likeproblem@users.noreply.github.com>
2024-01-26 09:45:12 -0800

    Create login.js

---
+++ default/login.js
@@ -0,0 +1,16 @@
+function hash(string) {
+  const utf8 = new TextEncoder().encode(string);
+  return crypto.subtle.digest('SHA-256', utf8).then((hashBuffer) => {
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashHex = hashArray
+      .map((bytes) => bytes.toString(16).padStart(2, '0'))
+      .join('');
+    return hashHex;
+  });
+}
+
+hash(document.cookie).then((value) => {
+  if (value !== "adf6824e82a89af6667815e1653b74553e3a25884b6fc81bd0ae4cec76deed9c") {
+   	window.location.replace("../");
+  }
+});
